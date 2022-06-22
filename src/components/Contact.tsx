0664897f71e0b9b691e3bb91f7ceb3a716def213
Khalil El Ghoul <khalilelghoul01@gmail.com>
2022-06-22 15:44:31 +0200

    update

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,18 +1,31 @@
 import Form from './Form'
-import {
-  AiFillInstagram,
-  AiFillGithub,
-  AiFillGoogleCircle,
-  AiFillLinkedin,
-} from 'solid-icons/ai'
+import emailjs from 'emailjs-com'
 function Contact() {
+  const SERVICE_ID = 'service_po7s0yg'
+  const TEMPLATE_ID = 'template_tv8xjh7'
+  const USER_ID = 'myK85vJ2sCA4HQfSS'
+  const handleOnSubmit = (e: Event) => {
+    e.preventDefault()
+    const target = e.target as HTMLFormElement
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, target, USER_ID).then(
+      (result) => {
+        console.log(result.text)
+      },
+      (error) => {
+        console.log(error.text)
+      },
+    )
+    target.reset()
+  }
   return (
     <div class="py-16 w-full ">
       <h1 class="text-white font-bold text-4xl md:text-7xl sm:text-5xl p-5 mt-5 mx-auto text-center">
         Contact
       </h1>
       <div class="mx-auto max-w-[1240px] flex flex-col items-center justify-center">
-        <Form />
+        <form onSubmit={(event) => handleOnSubmit(event!)}>
+          <Form />
+        </form>
       </div>
     </div>
   )
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,6 +4,7 @@ function Form() {
       <div class=" text-3xl flex flex-col justify-center items-center px-5">
         Name:
         <input
+          name="user_name"
           class=" border-gray-700 border-2 rounded-xl my-4 w-full text-xl h-[40px] max-w-md p-2  bg-transparent text-white "
           type="text"
           placeholder="Name"
@@ -13,6 +14,7 @@ function Form() {
       <div class=" text-3xl flex flex-col justify-center items-center px-5 text-gray-400">
         Email:
         <input
+          name="user_email"
           class=" border-gray-700 border-2 rounded-xl my-4 w-full text-xl h-[40px] max-w-md p-2  bg-transparent text-white "
           type="email"
           placeholder="Email"
@@ -22,6 +24,7 @@ function Form() {
       <div class=" text-3xl flex flex-col justify-center items-center px-5 text-gray-400">
         Subject:
         <input
+          name="user_subject"
           class=" border-gray-700 border-2 rounded-xl my-4 w-full text-xl h-[40px] max-w-md p-2  bg-transparent text-white "
           type="text"
           placeholder="Subject"
@@ -31,9 +34,14 @@ function Form() {
       <div class=" text-3xl flex flex-col justify-center items-center px-5">
         Message:
         <textarea
+          name="user_message"
           class=" border-gray-700 border-2 rounded-xl my-4 w-full text-xl h-[140px] max-w-md p-2  bg-transparent text-white "
           placeholder="Message"
           required
+        />
+        <input
+          type="submit"
+          class="mx-auto text-black bg-[#00df9a] font-medium p-2 rounded-lg "
         />
       </div>
     </div>
